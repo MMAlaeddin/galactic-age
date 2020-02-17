dc161db4d3142a8c951eaf537b0576b26a653ef4
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-02-17 08:10:43 -0800

    fix indent

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -1,79 +1,79 @@
 export class Age {
   constructor(earthAge, exercise) {
-    this.earthAge = earthAge,
-    this.exercise = exercise
+      this.earthAge = earthAge,
+          this.exercise = exercise
   }
 
   checkAgeInput() {
-    if (this.earthAge.includes("")) {
-      return true
-    }
+      if (this.earthAge.includes("")) {
+          return true
+      }
   }
   parseAge() {
-    let ageInput = parseInt(this.earthAge)
-    return ageInput
-    }
+      let ageInput = parseInt(this.earthAge)
+      return ageInput
+  }
 
   mercuryYears() {
-    let yearsOnMerc = parseInt(this.parseAge() / .24)
-    return yearsOnMerc
+      let yearsOnMerc = parseInt(this.parseAge() / .24)
+      return yearsOnMerc
   }
   venusYears() {
-    let yearsOnVenus = parseInt(this.parseAge() / .62)
-    return yearsOnVenus
+      let yearsOnVenus = parseInt(this.parseAge() / .62)
+      return yearsOnVenus
   }
 
   marsYears() {
-    let yearsOnMars = parseInt(this.parseAge() / 1.88)
-    return yearsOnMars
+      let yearsOnMars = parseInt(this.parseAge() / 1.88)
+      return yearsOnMars
   }
 
   jupiterYears() {
-    let yearsOnJupiter = parseInt(this.parseAge() / 11.86)
-    return yearsOnJupiter
+      let yearsOnJupiter = parseInt(this.parseAge() / 11.86)
+      return yearsOnJupiter
   }
 
   exerciseInput() {
-    let yesExercise = 5
-    let noExercise = 0
-    if (this.exercise === "yes") {
-      return yesExercise
-    } else if (this.exercise === "no") {
-      return noExercise
-    }
+      let yesExercise = 5
+      let noExercise = 0
+      if (this.exercise === "yes") {
+          return yesExercise
+      } else if (this.exercise === "no") {
+          return noExercise
+      }
   }
-  
+
   humanYears() {
-    let human = 79
-    return human
-  
+      let human = 79
+      return human
+
   }
   remainingYears() {
-    let remain = this.humanYears() - this.earthAge
-    let totalRemain = remain + this.exerciseInput() 
-    return totalRemain
+      let remain = this.humanYears() - this.earthAge
+      let totalRemain = remain + this.exerciseInput()
+      return totalRemain
   }
   totalMercury() {
-    let exerMerc = this.exerciseInput() + this.mercuryYears()
-    let totalMerc = exerMerc - this.humanYears()
-    return (exerMerc + " and you've lived " + totalMerc + " years past your expected age on Earth.")
+      let exerMerc = this.exerciseInput() + this.mercuryYears()
+      let totalMerc = exerMerc - this.humanYears()
+      return (exerMerc + " and you've lived " + totalMerc + " years past your expected age on Earth.")
   }
   totalVenus() {
-    let exerVenus = this.exerciseInput() + this.venusYears()
-    let totVenus = this.humanYears() - this.venusYears()
-    let totalVenus = totVenus - this.exerciseInput()
-    return (exerVenus + " and you've got " + totalVenus + " years left to live!")
-}
+      let exerVenus = this.exerciseInput() + this.venusYears()
+      let totVenus = this.humanYears() - this.venusYears()
+      let totalVenus = totVenus - this.exerciseInput()
+      return (exerVenus + " and you've got " + totalVenus + " years left to live!")
+  }
   totalMars() {
-    let exerMars = this.exerciseInput() + this.marsYears()
-    let totMars = this.humanYears() - this.marsYears()
-    let totalMars = totMars - this.exerciseInput()
-    return (exerMars + " and you've got " + totalMars + " years left to live!")
+      let exerMars = this.exerciseInput() + this.marsYears()
+      let totMars = this.humanYears() - this.marsYears()
+      let totalMars = totMars - this.exerciseInput()
+      return (exerMars + " and you've got " + totalMars + " years left to live!")
   }
   totalJupiter() {
-    let exerJupiter = this.exerciseInput() + this.jupiterYears()
-    let totJupiter = this.humanYears() - this.jupiterYears()
-    let totalJupiter = totJupiter - this.exerciseInput()
-    return (exerJupiter + " and you've got " + totalJupiter + " years left to live!")
-}
+      let exerJupiter = this.exerciseInput() + this.jupiterYears()
+      let totJupiter = this.humanYears() - this.jupiterYears()
+      let totalJupiter = totJupiter - this.exerciseInput()
+      return (exerJupiter + " and you've got " + totalJupiter + " years left to live!")
+  }
 }
